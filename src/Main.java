91f2be481daf94374c234962d82b7decf26bf887
Shashith-Malshan <malshanshashith318@gmail.com>
2025-12-11 15:56:37 +0530

    Add print Method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+
 public class Main {
+
+    //static String[] fruits={"apple","banana","orange"};
+    static String[] fruits=new String[0];
+
+    static void printFruits(){
+        //System.out.println(Arrays.toString(fruits));
+
+        System.out.print(fruits.length==0?"[null":"[");
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.print(fruits[i]+", ");
+        }
+        System.out.println(fruits.length==0?"]":"\b\b]");
+    }
+
+
+
     public static void main(String[] args) {
+        printFruits();
 
     }
 }
